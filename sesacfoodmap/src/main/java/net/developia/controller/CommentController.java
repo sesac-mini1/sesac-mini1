d05a9014bd0200b64bf6419de4588dd84627f281
Jaehyuk-Lee <alex00728@outlook.com>
2024-11-22 17:04:08 +0900

    add controller

---
+++ sesacfoodmap/src/main/java/net/developia/controller/CommentController.java
@@ -0,0 +1,11 @@
+package net.developia.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class CommentController {
+	
+}
