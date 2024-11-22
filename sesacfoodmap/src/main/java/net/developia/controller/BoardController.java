3926986849a571422473093dae710f92a4b951cd
jeeyun2 <irisjeeyun@naver.com>
2024-11-22 17:14:25 +0900

    boardController commit

--- sesacfoodmap/src/main/java/net/developia/controller/BoardController.java
+++ sesacfoodmap/src/main/java/net/developia/controller/BoardController.java
@@ -1,11 +1,15 @@
 package net.developia.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
+@RequestMapping("/board/")
 public class BoardController {
-	
+	@GetMapping("/get")
+	public void get() {}
 }
