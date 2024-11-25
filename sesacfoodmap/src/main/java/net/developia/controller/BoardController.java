63d859cce802c783e2a49fa7188bcf69ad65aeb9
jeeyun2 <irisjeeyun@naver.com>
2024-11-25 16:57:44 +0900

    get, BoardController get 관련

--- sesacfoodmap/src/main/java/net/developia/controller/BoardController.java
+++ sesacfoodmap/src/main/java/net/developia/controller/BoardController.java
@@ -1,15 +1,29 @@
 package net.developia.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
+import net.developia.service.BoardService;
 
 @Controller
 @Log4j
 @RequestMapping("/board/")
+@AllArgsConstructor
 public class BoardController {
+	private BoardService service;
+	
 	@GetMapping("/get")
-	public void get() {}
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		try {
+			log.info("/get");
+			model.addAttribute("board", service.get(bno));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
