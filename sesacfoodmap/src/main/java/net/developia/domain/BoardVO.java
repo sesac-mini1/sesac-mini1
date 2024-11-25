35ae5915e331c6ce282a4b2d3e3e44d80549bcd9
jeeyun2 <irisjeeyun@naver.com>
2024-11-25 15:12:32 +0900

    BoardVO 생성

---
+++ sesacfoodmap/src/main/java/net/developia/domain/BoardVO.java
@@ -0,0 +1,21 @@
+package net.developia.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+	private Long bno;
+	private String title;
+	private String content;
+	private String writer;
+	private String password;
+	private Date regDate;
+	private boolean ticket;
+	private String type;
+	private String filename;
+	private int stars;
+	private int recommend;
+	private int comments;
+}
