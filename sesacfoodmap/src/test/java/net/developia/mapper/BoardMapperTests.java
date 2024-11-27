5f335d9b76d795a788c83ca66888e7644e04ef6f
Lee Jaehyuk <alex00728@outlook.com>
2024-11-27 14:39:33 +0900

    Add like button (#19)
    * bug fixes
    
    * like ajax button
    
    * add css for like_button

--- sesacfoodmap/src/test/java/net/developia/mapper/BoardMapperTests.java
+++ sesacfoodmap/src/test/java/net/developia/mapper/BoardMapperTests.java
@@ -1,20 +1,14 @@
 package net.developia.mapper;
 
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import lombok.extern.log4j.Log4j;
-import net.developia.domain.BoardVO;
 import net.developia.domain.Criteria;
-import net.developia.service.BoardService;
 
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -23,12 +17,6 @@ import net.developia.service.BoardService;
 public class BoardMapperTests {
 	@Autowired
 	private BoardMapper mapper;
-	
-	@Autowired
-    private BoardService service;
-	
-	@Autowired
-    private MockMvc mockMvc;
 
 //	@Test
 //	public void testGet() {
@@ -38,19 +26,8 @@ public class BoardMapperTests {
 //	}ㅁ
 //
 	@Test
-	public void testGetList() throws Exception {
-//		List<BoardVO> boardList = mapper.getList();
-//		log.info(boardList);
-//		
-//		service.getList(new Criteria(2,10)).forEach(board -> log.info(board));
-		
-		log.info(mockMvc.perform(
-				MockMvcRequestBuilders.get("/board/list")
-				.param("pageNum", "2")
-				.param("amount", "50")
-				)
-				.andReturn().getModelAndView().getModelMap());
-
+	public void testGetListWithPaging() throws Exception {
+		mapper.getListWithPaging(new Criteria(2, 50)).forEach(board -> log.info(board));
 	}
 	
 	@Test
@@ -61,6 +38,11 @@ public class BoardMapperTests {
 //		
 //		List<BoardVO> list = mapper.getListWithPaging(cri);
 		
+	}
+	
+	@Test
+	public void testLikeUp() {
+		log.info(mapper.likeUp(1L));
 	}
 	
 }
